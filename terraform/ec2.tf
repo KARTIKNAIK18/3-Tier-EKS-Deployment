c322960fd8fff279c9b9961955ed8ccf7ead9fa5
KARTIKNAIK18 <kartiknaik560@gmail.com>
2025-08-10 16:00:28 +0530

    GitOps Files Adedd

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "aws-key-pair" {
-    key_name = "hotstar-pem-key"
+    key_name = "hotstar-terra-key"
     public_key = file("./terra-key.pub")
   
 }
@@ -45,6 +45,68 @@ resource "aws_security_group" "allow_user_to_connect" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Redis port allow"
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SMTPS port allow"
+    from_port   = 465
+    to_port     = 465
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  
+  }
+  
+
+  ingress {
+    description = "SMTP port allow"
+    from_port   = 25
+    to_port     = 25
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  
+  }
+
+
+  ingress {
+    description = "kubernetes API port allow"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  
+  }
+
+  ingress {
+    description = "kubernetes NodePort range allow"
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  
+  }
+
+  ingress {
+    description = "NodePort range allow"
+    from_port   = 3000
+    to_port     = 10000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  
+  }
+
+
+
+
+
+  
+  
+
   tags = {
     Name = "mysecurity"
   }
@@ -55,6 +117,7 @@ resource "aws_instance" "testinstance" {
   instance_type   = var.ec2_instance_type
   key_name        = aws_key_pair.aws-key-pair.key_name
   security_groups = [aws_security_group.allow_user_to_connect.name]
+  user_data = file("./run.sh")
   tags = {
     Name = "Automate"
   }
